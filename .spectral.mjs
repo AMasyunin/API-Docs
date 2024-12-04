93150858de9b52d2fa689063bccf9cbeec923211
a.masyunin@squaregps.com <a.masyunin@squaregps.com>
2024-12-04 17:27:46 +0100

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2NjIyMQ/spectral.js?branch_slug=master&token=83b5cd49-df60-4a76-af33-910658a25234";
+export default { extends: ruleset };
